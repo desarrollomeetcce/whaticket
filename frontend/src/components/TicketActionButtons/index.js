1b482357af14f9e67c9b019a186ec587c25fff37
desarrollomeetcce <desarrollo@agenciamagneto.org>
2021-06-02 19:27:21 -0500

    Se agrega select para cambiar ticket

--- frontend/src/components/TicketActionButtons/index.js
+++ frontend/src/components/TicketActionButtons/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useEffect,useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
